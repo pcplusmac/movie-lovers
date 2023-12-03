a206d1e66f53a1d303c1039d13fab0085a960806
pc-sam <samurai.h@yahoo.com>
2023-12-03 10:48:32 +0800

    add the functions to bring the data of films and dispaying and page in the pool area

--- index.js
+++ index.js
@@ -14,18 +14,34 @@ function formDataHandler(dataArr){
 
 let num = Math.floor(Math.random() * 82)
 function init() {
+    getPeople()
+    getFilms()
+    
+}
 
+function getPeople(){
     let request = `https://swapi.dev/api/people/`
     fetch(request)
         .then(resp => resp.json())
         .then(people => {
             
-            console.log(people,people.results)
-            renderPage(people.results) 
+            // console.log(people,people.results)
+            renderPeople(people.results) 
         })
 }
+function getFilms(){
+    let request = `https://swapi.dev/api/films/`
+    fetch(request)
+        .then(resp => resp.json())
+        .then(films => {
+            
+            // console.log(people,people.results)
+            renderFilms(films.results) 
+        })
 
-function renderPage(data) {
+}
+
+function renderPeople(data) {
     
     for (const person of data) {
 
@@ -47,6 +63,20 @@ function renderPage(data) {
     ulpeople.appendChild(character)
     }
 
+
+}
+
+function renderFilms(data){
+    // console.log(data)
+    data.forEach( film => {
+
+        let ulfilms = document.querySelector("#films-list")
+        let lifilm = document.createElement('li')
+        lifilm.innerText = film.title 
+        ulfilms.appendChild(lifilm)
+    }
+        
+    )
 }
 
 {/* <p>${person.height}</p>
