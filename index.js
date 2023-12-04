dd21344a5bde090b692b437e3d6800dc000020a3
pc-sam <samurai.h@yahoo.com>
2023-12-05 07:11:32 +0800

    add the background image for process-container, and some category's block-- person

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ function  confirmingFormHandler(event) {
 }
 
 function renderPerson(data){
-    const div = document.querySelector('.process-container') 
+    const div = document.querySelector('.process-container')
+    // div.style.backgroundImage = `url("https://images.unsplash.com/photo-1618336753974-aae8e04506aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3RhcndhcnN8ZW58MHx8MHx8fDA%3D")  `
     const h4 = document.createElement('h4')
     h4.textContent = "cast:"
     div.appendChild(h4)
